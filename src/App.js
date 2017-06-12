b4b6709c7e96aba2bcf74ee2e3c6dbf6089f5c51
Alex Henry <adhe223@g.uky.edu>
2017-06-11 19:29:16 -0700

    Basic styling. Next step is to reactify the nav bar.

--- src/App.js
+++ src/App.js
@@ -16,19 +16,34 @@ class App extends Component {
           </nav>
 
           <section id="section1">
-              Section 1
+              <p>
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+              </p>
           </section>
           <section id="section2">
-              Section 2
+              <p>
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+              </p>
           </section>
           <section id="section3">
-              Section 3
+              <p>
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+                  Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
+                  Prosciutto spare ribs rump pancetta kevin alcatra pig shoulder leberkas fatback beef ribs meatball venison pork belly. Picanha brisket jowl, capicola landjaeger turducken beef kielbasa pork loin filet mignon corned beef ground round ball tip turkey. Tri-tip chuck leberkas porchetta pork chop andouille. Picanha kevin pork chop salami shankle landjaeger swine pastrami chicken.
+              </p>
           </section>
 
           <footer>
-              <p>Contact information: </p>
+              Contact information:&nbsp;
               <a href="mailto:cindy.xn.han@gmail.com">cindy.xn.han@gmail.com</a>
-              <p> and </p>
+              &nbsp;and&nbsp;
               <a href="mailto:alex.david.henry@gmail.com">alex.david.henry@gmail.com</a>
           </footer>
       </div>
