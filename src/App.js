f82288630bef6cd0c2bf1d208bdaf64adab8467c
Alex Henry <adhe223@g.uky.edu>
2017-06-11 18:48:04 -0700

    Create main page skeleton

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <header>
+              <h1>C&A Consulting</h1>
+          </header>
+          <nav>
+              <a href="#section1">Section 1</a> |
+              <a href="#section2">Section 2</a> |
+              <a href="#section3">Section 3</a>
+          </nav>
+
+          <section id="section1">
+              Section 1
+          </section>
+          <section id="section2">
+              Section 2
+          </section>
+          <section id="section3">
+              Section 3
+          </section>
+
+          <footer>
+              <p>Contact information: </p>
+              <a href="mailto:cindy.xn.han@gmail.com">cindy.xn.han@gmail.com</a>
+              <p> and </p>
+              <a href="mailto:alex.david.henry@gmail.com">alex.david.henry@gmail.com</a>
+          </footer>
       </div>
     );
   }
