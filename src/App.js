78005cb378193c35bcbf53c969201c7e97ee3a7f
Alex Henry <adhe223@g.uky.edu>
2017-06-13 15:32:41 -0700

    Added photostream stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './component-library/components/NavBar';
+import PhotoStream from './component-library/components/PhotoStream';
 
 class App extends Component {
   render() {
@@ -15,6 +16,21 @@ class App extends Component {
       }
     ];
 
+    const photos = [
+        {
+            src: "https://cdn.thinglink.me/api/image/703259784207400961/230/230/thumbnail",
+            alt: "Puppy!"
+        },
+        {
+            src: "http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg",
+            alt: "Puppy!"
+        },
+        {
+            src: "https://vetstreet-brightspot.s3.amazonaws.com/fa/19/73e1a8864e8a868b30d728f81431/most-popular-puppy-names-2016-thinkstockphotos-527135315.jpg",
+            alt: "Puppy!"
+        },
+    ];
+
     return (
       <div className="App">
           <header>
@@ -22,6 +38,7 @@ class App extends Component {
           </header>
           <NavBar options={navOptions} />
 
+          <PhotoStream photos={photos} height="250px" />
           <section id="section1">
               <p>
                   Bacon ipsum dolor amet prosciutto spare ribs ground round, alcatra swine picanha fatback shankle pork flank beef ribs cupim. Shank corned beef shankle landjaeger flank pork chop brisket bacon alcatra short loin prosciutto salami ribeye pork loin fatback. Hamburger pork tongue pork loin. Tail andouille strip steak ham picanha burgdoggen. Sirloin pork chop hamburger, jowl pork belly burgdoggen beef ribs cow bacon ball tip capicola. Swine pork turducken ribeye tenderloin. Turducken ham cow fatback.
